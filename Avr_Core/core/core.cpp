ad21eda3d482d2e860752650c2f8a17ee223f9df
Anthony Bracegirdle <ant.braceg@gmail.com>
2013-02-21 12:49:03 +1300

    Windows code fixed

--- Avr_Core/core/core.cpp
+++ Avr_Core/core/core.cpp
@@ -34,7 +34,7 @@ int vasprintf(char **strp, const char *fmt, va_list ap)
         va_copy(aq, ap);
         ret = vsnprintf(NULL, 0, fmt, aq);
         va_end(aq);
-        if ((*strp = malloc(ret + 1)) == NULL)
+        if ((*strp = (char*)malloc(ret + 1)) == NULL)
                 return (-1);
         ret = vsnprintf(*strp, ret + 1, fmt, ap);
         return (ret);
@@ -138,10 +138,6 @@ std::string Avr_Core::debugFormat(const char *fmt, ...)
     va_list ap;
     va_start(ap, fmt);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
     if (!vasprintf(&ret, fmt, ap)){
         qDebug() << "Form debug string for queue failed";
     };
